89aeb06526b206e65fc0a32a15d57c875cd2985f
andrey <mishin23042002@gmai.com>
2024-11-16 15:38:34 +0300

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,8 @@ from app.routers.auth import auth_router
 from app.routers.profile import profile_router
 from app.routers.role import role_router
 from app.database import create_tables
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="User Management Service")
 
@@ -10,6 +12,13 @@ app.include_router(auth_router)
 app.include_router(profile_router)
 app.include_router(role_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить доступ с любых источников
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить любые HTTP-методы (GET, POST, и т.д.)
+    allow_headers=["*"],  # Разрешить любые заголовки
+)
 
 @app.on_event("startup")
 async def startup():
